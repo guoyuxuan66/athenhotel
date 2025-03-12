8be9a4b3e448dd5c51bd2681387904180ad0287b
guoyuxuan66 <81405332+guoyuxuan66@users.noreply.github.com>
2025-03-12 15:40:46 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // 滚动动画效果
-  const ctaButton = document.querySelector('.cta-button');
-  ctaButton.addEventListener('click', function(event) {
-    event.preventDefault();
-    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
-  });
-});
